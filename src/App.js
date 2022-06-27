7426bcbcfef64af408f065d53299367e82f87976
Gaurav <gaurav.kumar04@sap.com>
2022-06-27 16:50:59 -0400

    Inital commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Some Text
         </p>
         <a
           className="App-link"
